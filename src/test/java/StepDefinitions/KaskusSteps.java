0334017bef6bc673bfc92e10227b437e92c01021
dianprasetyo <dianprasetyo.eltoro@gmail.com>
2020-12-04 09:00:36 +0700

    add step definition and test runner

--- src/test/java/StepDefinitions/KaskusSteps.java
+++ src/test/java/StepDefinitions/KaskusSteps.java
@@ -1,13 +1,27 @@
 package StepDefinitions;
 
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 
-public class KaskusSteps {
+    public class KaskusSteps {
 
-     @Given("user access kaskus page")
+        @Given("user access kaskus page")
         public void userAccessKaskus(){
-        System.out.println("Hello world");
+
+            System.out.println("Inside step - access kaskus web");
         }
-}
+
+        @When("user click login button")
+        public void userClickLoginButton(){
+
+            System.out.println("Inside step - user click login");
+        }
+
+        @Then("user is on login form")
+        public void userIsOnLoginForm(){
+
+            System.out.println("Inside step - user is on login form");
+        }
+
+    }
--- target/test-classes/Features/login.feature
+++ target/test-classes/Features/login.feature
@@ -3,5 +3,5 @@ Feature: Test login kaskus
   @test_login_kaskus
   Scenario: user successfully login to kaskus
     Given user access kaskus page
-    And user click login button
+    When user click login button
     Then user is on login form
